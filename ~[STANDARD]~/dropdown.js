1d40d4e33df07750ce94994a9e23593783ee5787
StresseR <anyname6666@gmail.com>
2024-11-18 21:25:47 +0200

    ---Specification added---

--- ~[STANDARD]~/dropdown.js
+++ ~[STANDARD]~/dropdown.js
@@ -1,11 +1,14 @@
 const expand = {
 	"doc_main": `
 		<li><ts><a href="info/introduction.html">Introduction</a></ts></li>
-		<li><ts><a href="info/reserved.html">Reserved expressions</a></ts></li>
+		<li><ts><a href="info/reserved.html">Keywords and operators</a></ts></li>
 		<li><ts><a href="info/preprocessor.html">Preprocessor</a></ts></li>
+		<li><ts>Data types</ts></li>
 		<li><ts>Expressions</ts></li>
+		<li><ts>Evaluation order</ts></li>
 		<li><ts>Initialisation</ts></li>
-		<li><ts>Standard library</ts></li>`
+		<li><ts><a href="info/standard.html">Standard library</a></ts></li>
+		<li><ts>Example code</ts></li>`
 };
 
 let dropdown = (element, content_id) => {
